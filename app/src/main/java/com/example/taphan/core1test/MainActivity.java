2b9fa71bc3f7fae501e0879eea63d3386e4998bc
taphan <trucphan307@gmail.com>
2017-02-23 19:47:00 +0100

    Parse method to convert JSONObject to Map

--- app/src/main/java/com/example/taphan/core1test/MainActivity.java
+++ app/src/main/java/com/example/taphan/core1test/MainActivity.java
@@ -19,6 +19,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -36,15 +39,20 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Read fagkode from user input and find general information about the subject
-                String subject = inputText.getText().toString();
-                new JSONTask().execute("http://www.ime.ntnu.no/api/course/en/" + subject);
+            // Read fagkode from user input and find general information about the subject
+            String subject = inputText.getText().toString();
+            new JSONTask().execute("http://www.ime.ntnu.no/api/course/en/" + subject);
             }
         });
     }
 
     public class JSONTask extends AsyncTask<String, String,String> {
 
+        /**
+         * @param params = paramaters from execute(String... params)
+         * The first parameter is URL of JSON, the following parameters are search keywords
+         * @return value (String) if search is successful, null if otherwise
+         */
         @Override
         public String doInBackground(String... params) {
             HttpURLConnection connection = null;
@@ -66,20 +74,17 @@ public class MainActivity extends AppCompatActivity {
                 // Begin parsing JSON, choose JSON objects and arrays to get hold of correct info
                 String finalJson = buffer.toString();
                 JSONObject parentObject = new JSONObject(finalJson);
-
-                /*JSONObject requestObject = parentObject.getJSONObject("request"); // code+year gir course/tma41052016
-                String code = requestObject.getString("courseCode");
-                int year = requestObject.getInt("year");
-                */
-
-                JSONObject courseObject = parentObject.getJSONObject("course");
+                String result = searchJson(parentObject, "course", params); // params[] = url
+                Map<String, String> jsonMap = new HashMap<>();
+                return result;
+                /*JSONObject courseObject = parentObject.getJSONObject("course");
                 JSONArray educationalRoleArray = courseObject.getJSONArray("educationalRole");
                 JSONObject finalObject = educationalRoleArray.getJSONObject(0);
                 String lecturer = finalObject.getString("code");
                 JSONObject personObject = finalObject.getJSONObject("person");
                 String dispName = personObject.getString("displayName");
 
-                return lecturer + ": " + dispName; // buffer.toString() is the entire JSON
+                return lecturer + ": " + dispName; // buffer.toString() is the entire JSON*/
 
             } catch (MalformedURLException ex) {
                 Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
@@ -103,6 +108,58 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
 
+        /**
+         * Hjelpemetode for søking etter enkelt informasjon i JSON
+         * @param parentObject
+         * @param key - key to the JSONObject
+         * @return the result of search, null if unsuccessful
+         * @throws JSONException
+         */
+        private String searchJson(JSONObject parentObject, String key, String... searchkeys) throws JSONException {
+            // Choose to only handle information on courses, and not from cache or request
+            JSONObject currentObject = parentObject.getJSONObject(key);
+            Iterator iterator = currentObject.keys();
+            while(iterator.hasNext()) {
+                String nextKey = (String) iterator.next();
+                if(currentObject.get(nextKey) instanceof JSONObject) {
+                    System.out.println(nextKey);
+                    JSONObject nextObject = currentObject.getJSONObject(nextKey);
+
+                if(nextObject != null) {
+                    searchJson(nextObject,nextKey, searchkeys);
+                }}
+            }
+            return null;
+        }
+
+        /**
+         * Convert JSONObject into a map, can then iterate through map (must iterate twice, used as last option)
+         * @param json
+         * @param out
+         * @return a map containing all JSONObject
+         * @throws JSONException
+         */
+        private Map<String,String> parse(JSONObject json , Map<String,String> out) throws JSONException{
+            Iterator<String> keys = json.keys();
+            while(keys.hasNext()){
+                String key = keys.next();
+                String val = null;
+                if ( json.get(key) instanceof JSONObject ) {
+                    JSONObject value = json.getJSONObject(key);
+                    parse(value,out);
+                }
+
+                else {
+                    val = json.getString(key);
+                }
+
+                if(val != null){
+                    out.put(key,val);
+                }
+            }
+            return out;
+        }
+
         @Override
         protected void onPostExecute(String result) {
             // Execute our Json reader and store desired information in result
@@ -112,11 +169,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-   /* public void onClick(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }*/
 }
