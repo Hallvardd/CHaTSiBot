0882600e9d6b1b06fcc28f3e7285d4d007a5fa3a
taphan <trucphan307@gmail.com>
2017-03-21 13:30:19 +0100

    Made ProfActivity class

---
+++ app/src/main/java/com/example/taphan/core1/ProfActivity.java
@@ -0,0 +1,13 @@
+package com.example.taphan.core1;
+
+/**
+ * Created by taphan on 21.03.2017.
+ */
+
+public class ProfActivity {
+
+    /* En professor kan:
+    - Får tilgang til database
+
+    */
+}
