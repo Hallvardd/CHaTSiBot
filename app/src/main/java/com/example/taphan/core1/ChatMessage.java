aaa478e9da945a9444b622467f0089985598fa9f
taphan <trucphan307@gmail.com>
2017-03-22 23:24:08 +0100

    App crashed, test commit before deleting everything

---
+++ app/src/main/java/com/example/taphan/core1/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.example.taphan.core1;
+
+/**
+ * Created by taphan on 22.03.2017.
+ */
+
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left, String message) {
+        super();
+        this.left = left;
+        this.message = message;
+    }
+}
