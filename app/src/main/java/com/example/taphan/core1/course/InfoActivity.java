205a37ac5724d9965729cf033836a7ecc88d6bb9
Hallvardd <hallvste@stud.ntnu.no>
2017-03-28 11:29:01 +0200

    added methods for populating unansweredQuestion view.

--- app/src/main/java/com/example/taphan/core1/course/InfoActivity.java
+++ app/src/main/java/com/example/taphan/core1/course/InfoActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.taphan.core1.R;
 import com.example.taphan.core1.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by Charles on 23.03.2017.
@@ -17,11 +19,13 @@ public class InfoActivity extends AppCompatActivity{
 
     private Button signOutButton;
     private Button addCourseButton;
+    private TextView tv;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
-
+        tv = (TextView) findViewById(R.id.showuser);
+        tv.setText(LoginActivity.globalUser.getUser());
         addCourseButton = (Button) findViewById(R.id.add_course);
         signOutButton = (Button) findViewById(R.id.sign_out);
 
@@ -41,8 +45,5 @@ public class InfoActivity extends AppCompatActivity{
                 startActivity(intent);
             }
         });
-
     }
-
-
 }
--- app/src/main/java/com/example/taphan/core1/login/LoginActivity.java
+++ app/src/main/java/com/example/taphan/core1/login/LoginActivity.java
@@ -132,6 +132,7 @@ public class LoginActivity extends AppCompatActivity {
                         }
                     }
                 });
+
     }
 }
 
