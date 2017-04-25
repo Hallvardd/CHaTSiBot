e611d2ca8a5f8a474db5d8071ab9afd453b20edd
Hallvardd <hallvste@stud.ntnu.no>
2017-04-25 15:27:47 +0200

    Changed how Cloud NPL process input, and how paths to question are created. Syntax analysis now checks for verbs, while ignoring auxiliary verbs, and the verb: "be".

--- app/src/test/java/com/example/taphan/core1/UserTest.java
+++ app/src/test/java/com/example/taphan/core1/UserTest.java
@@ -19,11 +19,8 @@ public class UserTest{
     private static final String userTypeStudent = "Student";
     private static final String userTypeProfessor = "Professor";
     private static final String email = "test@email.com";
-    private boolean isTATrue = true;
-    private HashMap<String, ArrayList<String>> unansweredQuestions;
-    private HashMap<String, ArrayList<String>> answeredQuestions;
-    private ArrayList<Course> uCourses;
-    private ArrayList<Course> tCourses;
+    private static final boolean isTA = true;
+
 
     // Tests if the constructor sets the proper variables
     @Test
@@ -52,7 +49,104 @@ public class UserTest{
         Assert.assertNotNull(tCoursesField.get(testConstructorUser));
     }
 
-    //TODO:TEST GETTERS AND SETTERS:
+    @Test
+    public void testGetUserID() throws NoSuchFieldException, IllegalAccessException {
+        final User testGettersUser = new User();
+        final Field idGetterField = testGettersUser.getClass().getDeclaredField("userID");
+        idGetterField.setAccessible(true);
+        idGetterField.set(testGettersUser, userID);
+        Assert.assertEquals(testGettersUser.getUserID(), userID);
+    }
+
+    @Test
+    public void testSetUserID() throws NoSuchFieldException, IllegalAccessException {
+        final User testSettersUser = new User();
+        testSettersUser.setUserID(userID);
+        final Field userIDField = testSettersUser.getClass().getDeclaredField("userID");
+        userIDField.setAccessible(true);
+        Assert.assertEquals(userIDField.get(testSettersUser), userID);
+
+    }
+
+    @Test
+    public void testGetUserTypeProfessor() throws NoSuchFieldException, IllegalAccessException {
+        final User testGettersUser = new User();
+        final Field typeGetterField = testGettersUser.getClass().getDeclaredField("userType");
+        typeGetterField.setAccessible(true);
+        typeGetterField.set(testGettersUser, userTypeProfessor);
+        Assert.assertEquals(testGettersUser.getUserType(), userTypeProfessor);
+
+    }
+
+    @Test
+    public void testSetUserTypeProfessor() throws NoSuchFieldException, IllegalAccessException {
+        final User testSettersUser = new User();
+        testSettersUser.setUserType(userTypeProfessor);
+        final Field userTypeField = testSettersUser.getClass().getDeclaredField("userType");
+        userTypeField.setAccessible(true);
+        Assert.assertEquals(userTypeField.get(testSettersUser), userTypeProfessor);
+
+    }
+
+    @Test
+    public void testGetUserTypeStudent() throws NoSuchFieldException, IllegalAccessException {
+        final User testGettersUser = new User();
+        final Field typeGetterField = testGettersUser.getClass().getDeclaredField("userType");
+        typeGetterField.setAccessible(true);
+        typeGetterField.set(testGettersUser, userTypeStudent);
+        Assert.assertEquals(testGettersUser.getUserType(), userTypeStudent);
+
+    }
+
+    @Test
+    public void testSetUserTypeStudent() throws NoSuchFieldException, IllegalAccessException {
+        final User testSettersUser = new User();
+        testSettersUser.setUserType(userTypeStudent);
+        final Field userTypeField = testSettersUser.getClass().getDeclaredField("userType");
+        userTypeField.setAccessible(true);
+        Assert.assertEquals(userTypeField.get(testSettersUser), userTypeStudent);
+
+    }
+
+    @Test
+    public void testGetEmail() throws NoSuchFieldException, IllegalAccessException {
+        final User testGettersUser = new User();
+        final Field emailField = testGettersUser.getClass().getDeclaredField("email");
+        emailField.setAccessible(true);
+        emailField.set(testGettersUser, email);
+        Assert.assertEquals(testGettersUser.getEmail(), email);
+
+    }
+
+    @Test
+    public void testSetEmail() throws NoSuchFieldException, IllegalAccessException {
+        final User testSettersUser = new User();
+        testSettersUser.setEmail(email);
+        final Field emailField = testSettersUser.getClass().getDeclaredField("email");
+        emailField.setAccessible(true);
+        Assert.assertEquals(emailField.get(testSettersUser), email);
+
+    }
+
+    @Test
+    public void testGetIsTA() throws NoSuchFieldException, IllegalAccessException {
+        final User testGettersUser = new User();
+        final Field isTAField = testGettersUser.getClass().getDeclaredField("isTA");
+        isTAField.setAccessible(true);
+        isTAField.set(testGettersUser, isTA);
+        Assert.assertEquals(testGettersUser.getIsTa(), isTA);
+    }
+
+    @Test
+    public void testSetIsTA() throws IllegalAccessException, NoSuchFieldException {
+        final User testSettersUser = new User();
+        testSettersUser.setIsTa(isTA);
+        final Field isTAField = testSettersUser.getClass().getDeclaredField("isTA");
+        isTAField.setAccessible(true);
+        Assert.assertEquals(isTAField.get(testSettersUser), isTA);
+
+    }
+
 
     /*Functionality tests:
      *
@@ -168,26 +262,84 @@ public class UserTest{
         Assert.assertNotSame(testUser.getAnsweredQuestions(),testMap);
 
     }
-/*
+
     // Adding and removing courses from arrayList
     @Test
     public void testAddtCourse(){
-
+        User testUser = new User();
+        Course course1 = new Course("TEST101", "test1");
+        Course course2 = new Course("TEST102", "test2");
+        Course course3 = new Course("TEST103", "test3");
+        ArrayList<Course> testList = new ArrayList<>((List<Course>) Arrays.asList(course1, course2));
+        ArrayList<Course> assignList = new ArrayList<>((List<Course>) Arrays.asList(course1, course2));
+        // assigns list to testUser
+        testUser.settCourses(assignList);
+        // Attempts to add course already in list
+        testUser.addtCourse(course1);
+        Assert.assertEquals(testUser.gettCourses(),testList);
+        // Attempts to add new course not in list
+        testUser.addtCourse(course3);
+        Assert.assertNotSame(testUser.gettCourses(),testList);
+        Assert.assertTrue(testUser.gettCourses().contains(course3));
     }
 
     @Test
-    public void testAddsCourse(){
+    public void testAdduCourse(){
+        User testUser = new User();
+        Course course1 = new Course("TEST101", "test1");
+        Course course2 = new Course("TEST102", "test2");
+        Course course3 = new Course("TEST103", "test3");
+        ArrayList<Course> testList = new ArrayList<>((List<Course>) Arrays.asList(course1, course2));
+        ArrayList<Course> assignList = new ArrayList<>((List<Course>) Arrays.asList(course1, course2));
+        // assigns list to testUser
+        testUser.setuCourses(assignList);
+        // Attempts to add course already in list
+        testUser.adduCourse(course1);
+        Assert.assertEquals(testUser.getuCourses(),testList);
+        // Attempts to add new course not in list
+        testUser.adduCourse(course3);
+        Assert.assertNotSame(testUser.getuCourses(),testList);
+        Assert.assertTrue(testUser.getuCourses().contains(course3));
 
     }
 
     @Test
     public void testRemovetCourse(){
+        User testUser = new User();
+        Course course1 = new Course("TEST101", "test1");
+        Course course2 = new Course("TEST102", "test2");
+        Course course3 = new Course("TEST103", "test3");
+        ArrayList<Course> testList = new ArrayList<>((List<Course>) Arrays.asList(course1, course2));
+        ArrayList<Course> assignList = new ArrayList<>((List<Course>) Arrays.asList(course1, course2));
+        // assigns list to testUser
+        testUser.settCourses(assignList);
+        // Attempts to remove course not in list
+        testUser.removetCourse(course3);
+        Assert.assertEquals(testUser.gettCourses(),testList);
+        // Attempts to remove course in list
+        testUser.removetCourse(course2);
+        Assert.assertNotSame(testUser.gettCourses(),testList);
+        Assert.assertFalse(testUser.gettCourses().contains(course2));
 
     }
 
     @Test
-    public void testRemoveUCourse(){
+    public void testRemoveuCourse(){
+        User testUser = new User();
+        Course course1 = new Course("TEST101", "test1");
+        Course course2 = new Course("TEST102", "test2");
+        Course course3 = new Course("TEST103", "test3");
+        ArrayList<Course> testList = new ArrayList<>((List<Course>) Arrays.asList(course1, course2));
+        ArrayList<Course> assignList = new ArrayList<>((List<Course>) Arrays.asList(course1, course2));
+        // assigns list to testUser
+        testUser.setuCourses(assignList);
+        // Attempts to remove course not in list
+        testUser.removeuCourse(course3);
+        Assert.assertEquals(testUser.getuCourses(),testList);
+        // Attempts to remove course in list
+        testUser.removeuCourse(course2);
+        Assert.assertNotSame(testUser.getuCourses(),testList);
+        Assert.assertFalse(testUser.getuCourses().contains(course2));
 
     }
-*/
 }
