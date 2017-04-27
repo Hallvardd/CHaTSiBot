495200c1adf316f9da4e98b07892286cf272f5b6
Hallvardd <hallvste@stud.ntnu.no>
2017-04-27 02:49:05 +0200

    Two new unitTest for Question

--- app/src/test/java/com/example/taphan/core1/ProfActivtyTest/ProfActivityTest.java
+++ app/src/test/java/com/example/taphan/core1/ProfActivtyTest/ProfActivityTest.java
@@ -1,12 +1,15 @@
 package com.example.taphan.core1.ProfActivtyTest;
 
-import com.example.taphan.core1.user.User;
+import com.example.taphan.core1.ProfActivity;
+import com.example.taphan.core1.questionDatabase.Question;
 
-import org.junit.Rule;
+import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by hallvard on 26.04.17.
@@ -15,15 +18,22 @@ public class ProfActivityTest {
 
 
 
-    @Mock
-    User globalUser;
-
-    @Rule
-    public MockitoRule mockitoRule = MockitoJUnit.rule();
-
     @Test
     public void testAddAnswerToDatabase(){
 
     }
+
+    @Test
+    public void testSetQList() throws NoSuchFieldException, IllegalAccessException {
+        Question q1 = new Question("1","test101","test?");
+        Question q2 = new Question("2","test102","test?");
+        ArrayList<Question> questions = new ArrayList<>((List<Question>) Arrays.asList(q1,q2));
+        final ProfActivity testSetStudentListeners = new ProfActivity();
+        testSetStudentListeners.setQList(questions);
+        final Field setStudentField = testSetStudentListeners.getClass().getDeclaredField("qList");
+        setStudentField.setAccessible(true);
+        Assert.assertEquals(setStudentField.get(testSetStudentListeners),questions);
+    }
+
 
 }
